aa72c48897352160190fd6e5089a71e20b77b4d3
Sahil <80311646+Sahil-Bhor@users.noreply.github.com>
2023-05-26 04:35:04 +0530

    Create Addition.scala

---
+++ Group_B/Addition.scala
@@ -0,0 +1,11 @@
+object Addition {
+  def main(args: Array[String]): Unit = {
+
+    val num1 = 10
+    val num2 = 20
+    
+    val sum = num1 + num2
+
+    println("The sum of " + num1 + " and " + num2 + " is: " + sum)
+  }
+}
